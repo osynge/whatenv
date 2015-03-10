3d5e88f12045287f8ff0f1309ae4597845297f9f
Owen Synge <osynge@suse.com>
2015-03-10 20:20:45 +0100

    tabs are evil

--- osweint/environ.py
+++ osweint/environ.py
@@ -4,43 +4,43 @@ import os
 
 
 def getenviromentvars():
-	metadata = {}
-	session_id =  os.environ.get('BUILD_TAG')
-	if session_id:
-		metadata['JENKINS_BUILD_TAG'] = session_id
-	session_id =  os.environ.get('NODE_NAME')
-	if session_id:
-		metadata['JENKINS_NODE_NAME'] = session_id
-	session_id =  os.environ.get('WORKSPACE')
-	if session_id:
-		metadata['JENKINS_WORKSPACE'] = session_id
-	session_id =  os.environ.get('BUILD_URL')
-	if session_id:
-		metadata['JENKINS_BUILD_URL'] = session_id
-	session_id =  os.environ.get('JENKINS_URL ')
-	if session_id:
-		metadata['JENKINS_JENKINS_URL '] = session_id
-	session_id =  os.environ.get('JENKINS_JAVA_HOME')
-	if session_id:
-		metadata['JENKINS_JENKINS_JAVA_HOME'] = session_id
-	session_id =  os.environ.get('EXECUTOR_NUMBER')
-	if session_id:
-		metadata['JENKINS_EXECUTOR_NUMBER'] = session_id
+    metadata = {}
+    session_id =  os.environ.get('BUILD_TAG')
+    if session_id:
+        metadata['JENKINS_BUILD_TAG'] = session_id
+    session_id =  os.environ.get('NODE_NAME')
+    if session_id:
+        metadata['JENKINS_NODE_NAME'] = session_id
+    session_id =  os.environ.get('WORKSPACE')
+    if session_id:
+        metadata['JENKINS_WORKSPACE'] = session_id
+    session_id =  os.environ.get('BUILD_URL')
+    if session_id:
+        metadata['JENKINS_BUILD_URL'] = session_id
+    session_id =  os.environ.get('JENKINS_URL ')
+    if session_id:
+        metadata['JENKINS_JENKINS_URL '] = session_id
+    session_id =  os.environ.get('JENKINS_JAVA_HOME')
+    if session_id:
+        metadata['JENKINS_JENKINS_JAVA_HOME'] = session_id
+    session_id =  os.environ.get('EXECUTOR_NUMBER')
+    if session_id:
+        metadata['JENKINS_EXECUTOR_NUMBER'] = session_id
 
-	session_id =  os.environ.get('XAUTHLOCALHOSTNAME')
-	if session_id:
-		metadata['TERMINAL_XAUTHLOCALHOSTNAME'] = session_id
+    session_id =  os.environ.get('XAUTHLOCALHOSTNAME')
+    if session_id:
+        metadata['TERMINAL_XAUTHLOCALHOSTNAME'] = session_id
+    session_id =  os.environ.get('OS_USERNAME')
+    if session_id:
+        metadata['TERMINAL_OS_USERNAME'] = session_id
+    session_id =  os.environ.get('GPG_TTY')
+    if session_id:
+        metadata['TERMINAL_GPG_TTY'] = session_id
+    session_id =  os.environ.get('SSH_CONNECTION')
+    if session_id:
+        metadata['TERMINAL_SSH_CONNECTION'] = session_id
+    session_id =  os.environ.get('HOSTNAME')
+    if session_id:
+        metadata['HOSTNAME'] = session_id
 
-	session_id =  os.environ.get('OS_USERNAME')
-	if session_id:
-		metadata['TERMINAL_OS_USERNAME'] = session_id
-
-	session_id =  os.environ.get('GPG_TTY')
-	if session_id:
-		metadata['TERMINAL_GPG_TTY'] = session_id
-
-	session_id =  os.environ.get('SSH_CONNECTION')
-	if session_id:
-		metadata['TERMINAL_SSH_CONNECTION'] = session_id
-
-	return metadata
+    return metadata
