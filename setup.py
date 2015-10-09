b75b2843f5814520522a076c6930800de0da791a
Owen Synge <osynge@suse.com>
2015-10-09 10:41:11 -0400

    dd new custom target for tags
    Makes tags file to browse code well.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from osweint.__version__ import version
 from sys import version_info
-
+import os
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup, find_packages, Command
 except ImportError:
 	try:
-            from distutils.core import setup
+            from distutils.core import setup, Command
 	except ImportError:
-            from ez_setup import use_setuptools
+            from ez_setup import use_setuptools, Command
             use_setuptools()
             from setuptools import setup, find_packages
 import os
@@ -33,6 +33,33 @@ except ImportError:
     pass
 
 
+class CustomTags(Command):
+    user_options = []
+    def __init__(self, *args, **kwargs):
+
+
+        self.packaging = "default value for this option"
+
+        # 'finalized' records whether or not 'finalize_options()' has been
+        # called.  'finalize_options()' itself should not pay attention to
+        # this flag: it is the business of 'ensure_finalized()', which
+        # always calls 'finalize_options()', to respect/update it.
+        self.finalized = 0
+    def run(self):
+        import subprocess
+        cmd = "ctags -L -"
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True )
+        FILE_TYPE_LIST = ["py"]
+        codeRootDir = os.getcwd()
+        for root,dirs,files in os.walk(codeRootDir):
+            for file in files:
+                for file_type in FILE_TYPE_LIST:
+                    if file.split('.')[-1] == file_type:
+                        process.stdin.write('%s\n' %os.path.join(root,file))
+    def finalize_options(self):
+        pass
+
+
 setup(name=Application,
     version=version,
     description="""Simple set of scritps to build up and tear down clusters in an Open Stack enviroment.""",
@@ -42,7 +69,7 @@ setup(name=Application,
     install_requires=[
        "nose >= 1.1.0",
        "setuptools",
-        ],    
+        ],
     tests_require=[
         'coverage >= 3.0',
         'nose >= 1.1.0',
@@ -68,5 +95,8 @@ setup(name=Application,
             'ChangeLog',
             'steering_example.json',
             'whatenv_example.cfg']),
-        ]    
+        ],
+    cmdclass={
+            'tags': CustomTags
+        }
 )
