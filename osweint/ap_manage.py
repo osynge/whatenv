6f3472b0575defaf7ccadd48b4a84199cff0852d
Owen Synge <osynge@suse.com>
2015-10-26 17:38:10 +0000

    add debounce method

--- osweint/ap_manage.py
+++ osweint/ap_manage.py
@@ -29,6 +29,13 @@ def manage(args):
         controler.connect()
         controler.list()
         return 0
+
+    if args.debounce:
+        controler = nvclient_mvc.controler()
+        controler.read_config(cfg)
+        controler.connect()
+        controler.debounce(args.state)
+
     log.error("No action set")
     return 10
 
@@ -36,6 +43,7 @@ def subparser_manage(subparsers):
     manage_parser = subparsers.add_parser('manage', help='Manage session')
     manage_parser.add_argument('--session-list', action ='store_true',help='list sessions')
     manage_parser.add_argument('--instance-list', action ='store_true',help='list instances')
+    manage_parser.add_argument('--debounce', action ='store_true',help='debounce instances')
     manage_parser.set_defaults(
         func=manage,
         description=manage.__doc__,
